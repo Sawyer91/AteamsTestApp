12c45ffc1521dbd648b1124e912c9eb3129d140d
Dmitriy Tyurin <t.dimon911@gmail.com>
2017-10-31 16:45:49 +0300

    Add bottom navigation menu

--- app/src/main/java/com/sawyerd/ateamstest/MainActivity.java
+++ app/src/main/java/com/sawyerd/ateamstest/MainActivity.java
@@ -1,5 +1,6 @@
 package com.sawyerd.ateamstest;
 
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -19,6 +20,42 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.home:
+
+                        setTitle("Main");
+                        Main fragment = new Main();
+                        android.support.v4.app.FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.replace(R.id.content, fragment, "FragmentName");
+                        fragmentTransaction.commit();
+
+                        return true;
+
+                    case R.id.about_me:
+
+                        setTitle("About Me");
+                        About_me about_me = new About_me();
+                        android.support.v4.app.FragmentTransaction fragmentTransaction2 = getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction2.replace(R.id.content, about_me, "FragmentName");
+                        fragmentTransaction2.commit();
+
+                        return true;
+                }
+
+
+                return false;
+            }
+        });
+
+        setTitle("Main");
+        Main fragment = new Main();
+        android.support.v4.app.FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.content, fragment, "FragmentName");
+        fragmentTransaction.commit();
     }
 
     public void post(View view){
@@ -26,13 +63,13 @@ public class MainActivity extends AppCompatActivity {
         EditText editText = (EditText) findViewById(R.id.editPost);
         String message = editText.getText().toString();
         int n = Integer.parseInt(message);
-            if (n <= 100 & n != 0) {
-                intent.putExtra(EXTRA_MESSAGE, message);
-                startActivity(intent);
-            } else {
-                Toast toast = Toast.makeText(getApplicationContext(), "n Больше 100", Toast.LENGTH_SHORT);
-                toast.show();
-            }
+        if (n <= 100 & n != 0) {
+            intent.putExtra(EXTRA_MESSAGE, message);
+            startActivity(intent);
+        } else {
+            Toast toast = Toast.makeText(getApplicationContext(), "n Больше 100", Toast.LENGTH_SHORT);
+            toast.show();
+        }
     }
 
     public void comments(View view){
@@ -54,16 +91,16 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, DisplayUsersActivity.class);
         EditText editText = (EditText) findViewById(R.id.editUser);
         String message = editText.getText().toString();
-            intent.putExtra(EXTRA_MESSAGE, message);
-            startActivity(intent);
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 
     public void photo(View view){
         Intent intent = new Intent(this, DisplayPhotoActivity.class);
         EditText editText = (EditText) findViewById(R.id.editPhoto);
         String message = editText.getText().toString();
-            intent.putExtra(EXTRA_MESSAGE, message);
-            startActivity(intent);
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
 
     }
 
@@ -71,8 +108,10 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, DisplayTodoActivity.class);
         EditText editText = (EditText) findViewById(R.id.editToDo);
         String message = editText.getText().toString();
-            intent.putExtra(EXTRA_MESSAGE, message);
-            startActivity(intent);
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
 
     }
+
+
 }
